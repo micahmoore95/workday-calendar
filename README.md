2fed42880a20d4773bd9e6409d21a1baec15924b
Moore <mooreart95@gmail.com>
2022-05-11 15:22:03 -0600

    rows

---
+++ README.md
@@ -0,0 +1,8 @@
+Work Day Calendar
+
+
+Use this calendar to organize your day! You can put any appointments or reminders in their corresponding time slot to make sure you never forget something important!
+
+
+
+![](assets%5Cimages%5CScreenshot-1.jpg)
---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var currentTime = moment();
+var saveButton = document.querySelector("#save-button");
+
+//set up time blocks
+var hour9 = document.querySelector("#hour-9");
+var hour10 = document.querySelector("#hour-10");
+var hour11 = document.querySelector("#hour-11");
+var hour12 = document.querySelector("#hour-12");
+var hour13 = document.querySelector("#hour-13");
+var hour14 = document.querySelector("#hour-14");
+var hour15 = document.querySelector("#hour-15");
+var hour16 = document.querySelector("#hour-16");
+var hour17 = document.querySelector("#hour-17");
+//allow text editing
+//save button with local storage
+//delete button
+//check if time has passed
+
